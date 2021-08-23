79fd0cb30c2fefbfbedd1a79b77b85f79992193a
KaelanDuck <135414484+KaelanDuck@users.noreply.github.com>
2021-08-23 16:55:58 +1000

    Update tools

--- helper.ps1
+++ helper.ps1
@@ -30,22 +30,20 @@ try {
     # fixes python.org
     [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
-    
-
     Write-Output "Downloading mingw-get"
     Invoke-WebRequest -Uri "https://nchc.dl.sourceforge.net/project/mingw/Installer/mingw-get/mingw-get-0.6.2-beta-20131004-1/mingw-get-0.6.2-mingw32-beta-20131004-1-bin.zip" -OutFile "temp\mingw-get-setup.zip"
     Write-Output "Downloading gcc"
-    Invoke-WebRequest -Uri "https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-win32.zip" -OutFile "temp\gcc.zip"
+    Invoke-WebRequest -Uri "https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.07/gcc-arm-none-eabi-10.3-2021.07-win32.zip" -OutFile "temp\gcc.zip"
     Write-Output "Downloading build tools"
-    Invoke-WebRequest -Uri "https://github.com/gnu-mcu-eclipse/windows-build-tools/releases/download/v2.11-20180428/gnu-mcu-eclipse-build-tools-2.11-20180428-1604-win32.zip" -OutFile "temp\buildtools.zip"
+    Invoke-WebRequest -Uri "https://github.com/xpack-dev-tools/windows-build-tools-xpack/releases/download/v4.2.1-2/xpack-windows-build-tools-4.2.1-2-win32-x64.zip" -OutFile "temp\buildtools.zip"
     Write-Output "Downloading netcat"
     Invoke-WebRequest -Uri "https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip" -Outfile "temp\netcat.zip"
     Write-Output "Downloading python 3"
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.1/python-3.7.1-embed-win32.zip" -OutFile "temp\python.zip"
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.6/python-3.9.6-embed-amd64.zip" -OutFile "temp\python.zip"
     Write-Output "Downloading openocd"
-    Invoke-WebRequest -Uri "https://github.com/gnu-mcu-eclipse/openocd/releases/download/v0.10.0-10-20181020/gnu-mcu-eclipse-openocd-0.10.0-10-20181020-0522-win32.zip" -OutFile "temp\openocd.zip"
+    Invoke-WebRequest -Uri "https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v0.11.0-1/xpack-openocd-0.11.0-1-win32-x64.zip" -OutFile "temp\openocd.zip"
     Write-Output "Downloading git"
-    Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.19.1.windows.1/MinGit-2.19.1-32-bit.zip" -OutFile "temp\git.zip"
+    Invoke-WebRequest -Uri "https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/MinGit-2.31.1-busybox-64-bit.zip" -OutFile "temp\git.zip"
 
     Expand-Archive -Path "temp\mingw-get-setup.zip" -DestinationPath "temp\mingw"
 
@@ -54,15 +52,17 @@ try {
 
     New-Item -ItemType Directory -Name "toolchain" > $null
 
-    Expand-Archive -Path "temp\gcc.zip" -DestinationPath "toolchain\gcc-arm"
+    Expand-Archive -Path "temp\gcc.zip" -DestinationPath "temp"
+    Move-Item -Path "temp\gcc-arm-none-eabi-10.3-2021.07" -Destination "toolchain\gcc-arm"
     Expand-Archive -Path "temp\buildtools.zip" -DestinationPath "temp"
-    Move-Item -Path "temp\GNU MCU Eclipse\Build Tools\2.11-20180428-1604\bin" -Destination "toolchain\build-tools"
+    Move-Item -Path "temp\xpack-windows-build-tools-4.2.1-2\bin" -Destination "toolchain\build-tools"
     Move-Item -Path "temp\mingw\msys\1.0" -Destination "toolchain\msys"
     Expand-Archive -Path "temp\netcat.zip" -DestinationPath "temp\netcat"
     Move-Item -Path "temp\netcat\nc.exe" "toolchain\build-tools"
     Expand-Archive -Path "temp\python.zip" -DestinationPath "toolchain\python"
+    Copy-Item -Path "toolchain\python\python.exe" -Destination "toolchain\python\python3.exe" # libopencm3 requires 'python3'
     Expand-Archive -Path "temp\openocd.zip" -DestinationPath "temp\openocd"
-    Move-Item -Path ".\temp\openocd\GNU MCU Eclipse\OpenOCD\0.10.0-10-20181020-0522" -Destination "toolchain\openocd"
+    Move-Item -Path ".\temp\openocd\xpack-openocd-0.11.0-1\" -Destination "toolchain\openocd"
     Expand-Archive -Path "temp\git.zip" -DestinationPath "toolchain\git"
 
     Remove-Item -Recurse "temp" > $null
